2adf21cf238501810553eed5c8310175d038e651
youngin-son <youngin.son@navercorp.com>
2022-03-10 13:38:47 +0900

    :sparkles: done Part04Transform - capitalize

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -1,5 +1,7 @@
 package io.pivotal.literx;
 
+import java.util.Locale;
+
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,14 +17,28 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map((User user) -> {
+			User caplitalUser = new User(
+				user.getUsername().toUpperCase(Locale.ROOT),
+				user.getFirstname().toUpperCase(Locale.ROOT),
+				user.getLastname().toUpperCase(Locale.ROOT)
+			);
+			return caplitalUser;
+		});
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map((User user) -> {
+			User caplitalUser = new User(
+				user.getUsername().toUpperCase(Locale.ROOT),
+				user.getFirstname().toUpperCase(Locale.ROOT),
+				user.getLastname().toUpperCase(Locale.ROOT)
+			);
+			return caplitalUser;
+		});
 	}
 
 //========================================================================================
