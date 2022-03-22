52ec9ca5cb4d023adeb6601bdd9008bd42b1e722
broccolism <mile3880@gmail.com>
2022-03-22 15:33:54 +0900

    :wrench: stream, javers 폴더 분리

---
+++ get-closer-with-javers/src/main/java/com/broccoli/javers/JaversApplication.java
@@ -0,0 +1,13 @@
+package com.broccoli.javers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaversApplication.class, args);
+	}
+
+}
---
+++ get-closer-with-javers/src/test/java/com/broccoli/javers/JaversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broccoli.javers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
