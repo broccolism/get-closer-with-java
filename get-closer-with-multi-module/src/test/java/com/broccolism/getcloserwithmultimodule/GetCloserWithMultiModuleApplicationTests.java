3e8d611315424b1ab2db6d9c025b70e5199d65f1
broccolism <mile3880@gmail.com>
2022-07-02 22:25:23 +0900

    :sparkles: multi module 테스트용 프로젝트

---
+++ get-closer-with-multi-module/src/test/java/com/broccolism/getcloserwithmultimodule/GetCloserWithMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broccolism.getcloserwithmultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCloserWithMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
