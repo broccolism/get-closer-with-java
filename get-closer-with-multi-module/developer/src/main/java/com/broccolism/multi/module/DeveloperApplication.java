9d5bb4eb3aeb7cffdf840c2269b4864fe11f1051
broccolism <mile3880@gmail.com>
2022-07-02 23:56:54 +0900

    :sparkles: developer application

---
+++ get-closer-with-multi-module/developer/src/main/java/com/broccolism/multi/module/DeveloperApplication.java
@@ -0,0 +1,11 @@
+package com.broccolism.multi.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperApplication.class, args);
+    }
+}
